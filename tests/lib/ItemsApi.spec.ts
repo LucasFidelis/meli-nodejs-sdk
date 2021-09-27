ff1142e353e3bff4a3bf7e8eef1e857e14928651
Lucas Fidelis <lucasfidelis97@gmail.com>
2021-09-27 18:06:53 -0300

    fix: itemIdGet to getItemById

--- tests/lib/ItemsApi.spec.ts
+++ tests/lib/ItemsApi.spec.ts
@@ -5,7 +5,7 @@ describe('ItemsApi', () => {
   it('Should return 200 and a data object in the body with a property id', async () => {
     const apiClient = new ApiClient()
     const itemsApi = new ItemsApi(apiClient)
-    const response = await itemsApi.itemIdGet('MLB1191972200')
+    const response = await itemsApi.getItemById('MLB1191972200')
     expect(response.statusCode).toBe(200)
     expect(response.body.id).toBe('MLB1191972200')
   })
