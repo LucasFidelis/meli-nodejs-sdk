b47d19ca27a7564e3d44fec701da27bd6b9f5932
Lucas Fidelis <lucasfidelis97@gmail.com>
2021-10-01 17:15:00 -0300

    test: ensure CategoriesApi.getDetailsById returns status code 200 and a property valid

---
+++ tests/lib/CategoriesApi.spec.ts
@@ -0,0 +1,16 @@
+import { ApiClient } from '../../src/lib/ApiClient'
+import { CategoriesApi } from '../../src/lib/CategoriesApi'
+import dotenv from 'dotenv'
+dotenv.config({ path: '../../.env'})
+
+describe('CategoriesApi', () => {
+  describe('getDetailsById', () => {
+    it('Should return 200 and a data object in the body with a property name', async () => {
+      const apiClient = new ApiClient()
+      const categoriesApi = new CategoriesApi(apiClient)
+      const response = await categoriesApi.getDetailsById('MLA1055')
+      expect(response.statusCode).toBe(200)
+      expect(response.body.name).toBe('Celulares y Smartphones')
+    })
+  })
+})
